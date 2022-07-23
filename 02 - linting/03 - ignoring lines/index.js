c71b6e09b14b99dd3b71ea877a071908af7b0822
cferdinandi <chris@gomakethings.com>
2022-07-23 18:11:19 -0400

    Initial commit

---
+++ 02 - linting/03 - ignoring lines/index.js
@@ -0,0 +1,10 @@
+// This would normally throw an error because of the missing semicolon
+let name = 'Merlin' // jshint ignore:line
+
+// This will throw an error
+let job = 'Wizard'
+
+/* jshint ignore:start */
+// This will be ignored
+let spell = 'Dancing teacups'
+/* jshint ignore:end */
